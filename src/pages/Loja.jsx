d98e753914911f8a151f9fff483a25ac1d1d1644
Nuuvsen <nuuvsen@gmail.com>
2026-05-27 22:52:38 -0300

    Atualização do App Trufa - 27/05/2026 22:52

--- src/pages/Loja.jsx
+++ src/pages/Loja.jsx
@@ -42,7 +42,7 @@ export default function Loja() {
     if (isCheckoutOpen && passoCheckout === 'pix') {
       const checarPagamento = async () => {
         try {
-          const resposta = await fetch('http://localhost:3001/api/status-pix');
+          const resposta = await fetch('http://systen.nuuvsen.com.br:3001/api/status-pix');
           const dados = await resposta.json();
 
           // Se o bot responder que a IA aprovou a imagem...
@@ -67,7 +67,7 @@ export default function Loja() {
       return () => clearInterval(intervalo);
     }
   }, [isCheckoutOpen, passoCheckout]);
-  
+
   const mudarQuantidade = (produtoId, alteracao) => {
     setCarrinho(prev => {
       const atual = prev[produtoId] || 0;
@@ -106,7 +106,7 @@ export default function Loja() {
     setPassoCheckout('pix'); 
 
     try {
-        await fetch('http://localhost:3001/api/iniciar-pagamento', {
+        await fetch('http://systen.nuuvsen.com.br:3001/api/iniciar-pagamento', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
